8d96814bdbdd2f838cdf7adf20f3839b1a77874c
mikbry <mikbry@aerolitec.com>
2017-12-15 18:53:21 +0100

    Expose functions in index.js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) 2015-present, CWB SAS
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import StringTools from "./helpers/stringTools";
+import FileTools from "./helpers/fileTools";
+import RandomStringGenerator from "./helpers/randomStringGenerator";
+import Password from "./helpers/password";
+import dbCreate from "./database";
+
+export { StringTools, FileTools, Password, RandomStringGenerator, dbCreate };
